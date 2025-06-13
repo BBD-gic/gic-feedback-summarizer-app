b3d7e4283bf4c869d19a31053001e3ae5c9643b5
BBD-gic <gic@betterbydesign.in>
2025-06-13 12:32:05 +0530

    Update server.js for dynamic port

--- server/server.js
+++ server/server.js
@@ -56,7 +56,7 @@ app.post("/generate-summary", async (req, res) => {
 });
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
