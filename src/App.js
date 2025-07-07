4e63b33fa12e2b04a223df5232c835c4e64a3fb1
BBD-gic <gic@betterbydesign.in>
2025-07-07 16:19:47 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
 
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Index from "./pages/Index";
+import DiveDeep from "./pages/DiveDeep";
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/deep-dive" element={<DiveDeep />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
